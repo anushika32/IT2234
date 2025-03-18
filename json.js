f1cec1a79556d5623da21c036f2c26dec8c1dc4f
anushika32 <anushikawijerathna00@gmail.com>
2025-03-18 12:35:20 +0530

    JSON performance

---
+++ json.js
@@ -0,0 +1,19 @@
+//JSON
+//{key:value}
+
+let student = {regno:'2021/ict/32',name:'Anushika',age:25,course:'IT', skills:['java',"JS",'C++']};
+console.log(student);
+console.log(student.name);
+let students=[
+   {regno:'2021/ict/32',name:'Anushika',age:25,course:'IT'},
+   {regno:'2021/ict/33',name:'Nuwani',age:23,course:'IT'},
+   {regno:'2021/ict/35',name:'Wenura',age:25,course:'IT'}
+   
+ ];
+ console.log(students);
+ 
+ //define 10 students JSON 
+ //store it in an array
+ //find thw female students
+ //find the students who are following IT course
+ //find the max and average GPA among the students
