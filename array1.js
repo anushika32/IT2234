9b943a164add6b98f8420735f07b34b4506d9ca6
anushika32 <anushikawijerathna00@gmail.com>
2025-03-18 12:30:31 +0530

    array performance

---
+++ array1.js
@@ -0,0 +1,17 @@
+//functions
+//array
+
+let numarry = [1,4,5,3]
+console.log(numarry);
+/*for(let i=0;i<numarry.length;i++)
+{
+	console.log(numarry[i])
+}
+*/
+numarry.forEach((n) =>
+{
+ console.log(n)
+}
+)
+
+
